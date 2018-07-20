0867f8eeac84fc999e713b21b7fa8c96eae42380
Mohammed Daudali <mdaudali786@gmail.com>
2018-07-20 16:10:45 +0100

    Optimised Problem 36

--- Problem 36.py
+++ Problem 36.py
@@ -1,4 +1,5 @@
 from utils.utils import is_palindrome
 
-double_base_palindromes = [i for i in range(1, 1000000) if is_palindrome(str(i)) and is_palindrome("{0:b}".format(i))]
+double_base_palindromes = [i for i in range(1, 1000000, 2) if is_palindrome(str(i)) and is_palindrome("{0:b}".format(i))]
 print(sum(double_base_palindromes))
+# ! you can step by 2 as even numbers will be of the form 1...0 which is definitely not a palindrome
